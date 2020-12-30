a00db12264b6b4faa3a71d627baf9f1f7b4c369f
Hajime Takeuchi <shadowthe8156@gmail.com>
2020-12-31 01:56:24 +0900

    styled-components 導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,40 @@
-import '../styles/globals.css';
-
+import { createGlobalStyle } from 'styled-components';
 import type { AppProps } from 'next/app';
 
+export const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,   Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+  
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+  
+  * {
+    box-sizing: border-box;
+  }
+`;
+
+const BasicLayout = ({ children }: { children: any }) => {
+  return (
+    <>
+      <GlobalStyle />
+      {children}
+    </>
+  );
+};
+
 const MyApp = (props: AppProps) => {
-  return <props.Component {...props.pageProps} />;
+  return (
+    <BasicLayout>
+      <props.Component {...props.pageProps} />
+    </BasicLayout>
+  );
 };
 
 export default MyApp;
