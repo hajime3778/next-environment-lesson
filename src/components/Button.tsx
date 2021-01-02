98615ca5b52b9935f069a6cdf6aed8ccb1370620
Hajime Takeuchi <shadowthe8156@gmail.com>
2021-01-02 19:36:11 +0900

    Buttonをstyled-components使用に変更、いらないstoryを削除

---
+++ src/components/Button.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+
+const StyledButton = styled.a<ButtonStyleProps>`
+  font-family: 'Nunito Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  font-weight: 700;
+  border: 0;
+  border-radius: 3em;
+  cursor: pointer;
+  display: inline-block;
+  line-height: 1;
+  ${(props: ButtonStyleProps) =>
+    props.primary
+      ? css`
+          color: white;
+          background-color: #1ea7fd;
+        `
+      : css`
+          color: #333;
+          background-color: transparent;
+          box-shadow: rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset;
+        `};
+  background: ${(props: ButtonStyleProps) => props.backgroundColor};
+  ${(props: ButtonStyleProps) =>
+    props.size === 'small'
+      ? css`
+          font-size: 12px;
+          padding: 10px 16px;
+        `
+      : props.size === 'large'
+      ? css`
+          font-size: 16px;
+          padding: 12px 24px;
+        `
+      : css`
+          font-size: 14px;
+          padding: 11px 20px;
+        `}
+`;
+
+export type ButtonStyleProps = {
+  primary?: boolean;
+  backgroundColor?: string;
+  size?: 'small' | 'medium' | 'large';
+};
+
+export type ButtonProps = {
+  label: string;
+  onClick?: () => void;
+} & ButtonStyleProps;
+
+export const Button: React.FC<ButtonProps> = ({
+  primary = false,
+  size = 'medium',
+  backgroundColor,
+  label,
+  ...props
+}) => {
+  return (
+    <StyledButton primary={primary} size={size} backgroundColor={backgroundColor} {...props}>
+      {label}
+    </StyledButton>
+  );
+};
